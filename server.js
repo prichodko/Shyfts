1aef2ef3896525e50fe73667780469e8de0e490d
Pavel Prichodko <prichodko.p@gmail.com>
2016-08-29 20:33:40 +0200

    Refactor main server.js file

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ require('dotenv').load()
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const mongoose = require('./lib/mongoose')
+const log = require('log4js').getLogger('app')
 
 const app = express()
 
-const router = require('./src/routes')
+const router = require('./src/api')
 
+mongoose.connect()
+
+app.set('port', process.env.PORT || 3000)
+app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api', router)
 
-const port = process.env.PORT || 3000
+log.info(app.get('env'))
 
-app.listen(port, function () {
-  console.log('Server listening on port ' + port)
+app.listen(app.get('port'), function () {
+  log.info('Listening on port: ' + app.get('port'))
 })
+
