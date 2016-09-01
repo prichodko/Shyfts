8d60be33045b9854640abca609ee352a429d7f66
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-01 10:44:02 +0200

    Update user model

--- src/User/user.model.js
+++ src/User/user.model.js
@@ -1,17 +1,56 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const hash = require('../../lib/hash')
+// const bcrypt = require('bcrypt')
+
 
 const UserSchema = new mongoose.Schema({
-  firstName: {},
-  lastName: {},
-  email: {},
-  username: {},
-  password: {}
+  firstName: { type: String },
+  lastName: { type: String },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 }, {
   timestamps: true
 })
 
-module.exports = mongoose.model('User', UserSchema)
 
-const express = require('express')
-const fs = require('fs')
+UserSchema.pre('save', function (next) {
+  const user = this
+
+  if (!user.isModified('password')) return next() // why?
+
+  hash.get(user.password, function (err, hash) {
+    if (err) next(err)
+
+    user.password = hash
+    next()
+  })
+})
+
+// Safely store a password using bcrypt hashing function
+// UserSchema.pre('save', function (next) {
+//   const user = this
+
+//   if (!user.isModified('password')) return next() // why?
+
+//   bcrypt.genSalt(function (err, salt) {
+//     if (err) next(err)
+
+//     bcrypt.hash(user.password, salt, function (err, encrypted) {
+//       if (err) next(err)
+
+//       user.password = encrypted
+//       next()
+//     })
+//   })
+// })
+
+
+module.exports = mongoose.model('User', UserSchema)
