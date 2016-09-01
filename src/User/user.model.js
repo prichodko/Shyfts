45426fa7947d2c8b19d1d010714803efad4cea44
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-01 12:53:48 +0200

    Email validation

--- src/User/user.model.js
+++ src/User/user.model.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+// const Joi = require('joi')
+const isemail = require('isemail')
+
 const hash = require('../../lib/hash')
 // const bcrypt = require('bcrypt')
-
 
 const UserSchema = new mongoose.Schema({
   firstName: { type: String },
@@ -10,7 +12,11 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: {
+      validator: email => isemail.validate(email),
+      message: 'Invalid email address.'
+    }
   },
   password: {
     type: String,
@@ -19,7 +25,6 @@ const UserSchema = new mongoose.Schema({
 }, {
   timestamps: true
 })
-
 
 UserSchema.pre('save', function (next) {
   const user = this
@@ -33,24 +38,5 @@ UserSchema.pre('save', function (next) {
     next()
   })
 })
-
-// Safely store a password using bcrypt hashing function
-// UserSchema.pre('save', function (next) {
-//   const user = this
-
-//   if (!user.isModified('password')) return next() // why?
-
-//   bcrypt.genSalt(function (err, salt) {
-//     if (err) next(err)
-
-//     bcrypt.hash(user.password, salt, function (err, encrypted) {
-//       if (err) next(err)
-
-//       user.password = encrypted
-//       next()
-//     })
-//   })
-// })
-
 
 module.exports = mongoose.model('User', UserSchema)
